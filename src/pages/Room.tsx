b0c461c8037bdcff1daa8e67f9ac29252d188aae
Marcos <marcosdoriguetto@gmail.com>
2021-06-24 21:11:18 -0300

    Criação do fluxo de enviar pergunta

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -6,20 +6,58 @@ import logoImg from '../assets/images/logo.svg'
 
 import '../styles/room.scss'
 import { RoomCode } from "../components/RoomCode";
+import { FormEvent, useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
+import toast, { Toaster } from 'react-hot-toast';
 
 type RoomParams = {
   id: string;
 }
 
 export function Room() {
+  const { user } = useAuth()
   const params = useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState('');
+
+  const roomId = params.id;
+
+  async function handleSendQuestion(event: FormEvent) {
+    event?.preventDefault();
+
+    if (newQuestion.trim() === '') {
+      return toast('Você precisa inserir uma pergunta', {
+        icon: '👀'
+      })
+    }
+
+    if (!user) {
+      throw toast.error('Você precisa estar logado!');
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar
+      },
+      isHighlighted: false,
+      isAnswerer: false
+    }
+
+    await database.ref(`rooms/${roomId}/questions`).push(question)
+    setNewQuestion('')
+
+    toast.success('Sua pergunta foi enviada com sucesso!')
+  }
 
   return (
     <div id="page-room">
+      <Toaster />
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <RoomCode code={params.id} />
+          <RoomCode code={roomId} />
         </div>
       </header>
 
@@ -29,12 +67,23 @@ export function Room() {
           <span>4 perguntas</span>
         </div>
 
-        <form action="">
-          <textarea placeholder="O que você quer perguntar" />
+        <form onSubmit={handleSendQuestion}>
+          <textarea
+            placeholder="O que você quer perguntar"
+            onChange={event => setNewQuestion(event.target.value)}
+            value={newQuestion}
+          />
 
           <div className="form-footer">
-            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-            <Button type="submit">Enviar pergunta</Button>
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt="Avatar da conta do Google" />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            )}
+            <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>
       </main>
