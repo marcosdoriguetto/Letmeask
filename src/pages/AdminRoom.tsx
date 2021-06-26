fed6852f704349e638a963eb78c39f2281624b79
Marcos <marcosdoriguetto@gmail.com>
2021-06-25 22:14:40 -0300

    Estilização do botão do like

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,8 +1,5 @@
 import { useParams } from "react-router-dom";
-import { FormEvent, useState } from "react";
-import { useAuth } from "../hooks/useAuth";
 import { useRoom } from "../hooks/useRoom";
-import { database } from "../services/firebase";
 
 import { Button } from "../components/Button";
 import { RoomCode } from "../components/RoomCode";
@@ -11,54 +8,20 @@ import logoImg from '../assets/images/logo.svg'
 
 import '../styles/room.scss'
 import { Question } from "../components/Question";
-import toast, { Toaster } from 'react-hot-toast';
-
 
 type RoomParams = {
   id: string;
 }
 
 export function AdminRoom() {
-  const { user } = useAuth()
   const params = useParams<RoomParams>();
-  const [newQuestion, setNewQuestion] = useState('');
 
   const roomId = params.id;
 
   const { title, questions } = useRoom(roomId);
 
-  async function handleSendQuestion(event: FormEvent) {
-    event.preventDefault();
-
-    if (newQuestion.trim() === '') {
-      return toast('Você precisa inserir uma pergunta', {
-        icon: '👀'
-      })
-    }
-
-    if (!user) {
-      throw toast.error('Você precisa estar logado!');
-    }
-
-    const question = {
-      content: newQuestion,
-      author: {
-        name: user.name,
-        avatar: user.avatar
-      },
-      isHighlighted: false,
-      isAnswerer: false
-    }
-
-    await database.ref(`rooms/${roomId}/questions`).push(question)
-    setNewQuestion('')
-
-    toast.success('Sua pergunta foi enviada com sucesso!')
-  }
-
   return (
     <div id="page-room">
-      <Toaster />
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
